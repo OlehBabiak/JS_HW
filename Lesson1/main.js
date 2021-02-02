bc7f2eb77fdece7e5915d982046db725164253c3
OlehBabiak <oleg_bob@ukr.net>
2021-02-02 13:57:52 +0200

    first commit

---
+++ Lesson1/main.js
@@ -0,0 +1,232 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//  Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// let greet = 'hello';
+// console.log(greet);
+// alert(greet)
+// document.write(greet)
+
+// let owu = 'owu';
+// console.log(owu);
+// alert(owu)
+// document.write(owu)
+
+// let domainName1 = 'com';
+// console.log(domainName1);
+// alert(domainName1)
+// document.write(domainName1)
+
+// let domainName2 = 'ua';
+// console.log(domainName2);
+// alert(domainName2)
+// document.write(domainName2)
+
+// let number1 = 1;
+// console.log(number1);
+// alert(number1)
+// document.write(number1)
+
+// let number10 = 10;
+// console.log(number10);
+// alert(number10)
+// document.write(number10)
+
+// let number = -999;
+// console.log(number);
+// alert(number)
+// document.write(number)
+
+// let number123 = 123;
+// console.log(number123);
+// alert(number123)
+// document.write(number123)
+
+// let numberPi = 3.14;
+// console.log(numberPi);
+// alert(numberPi)
+// document.write(numberPi)
+
+// let numberWithDot = 2.7;
+// console.log(numberE);
+// alert(numberE)
+// document.write(numberE)
+
+// let boolTrue = true;
+// console.log(boolTrue);
+// alert(boolTrue)
+// document.write(boolTrue)
+
+// let boolfalse = false;
+// console.log(boolfalse);
+// alert(boolfalse)
+// document.write(boolfalse)
+
+// Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
+//   Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// greet = 'bye';
+// console.log(greet);
+// alert(greet)
+// document.write(greet)
+
+// owu = 'rto';
+// console.log(owu);
+// alert(owu)
+// document.write(owu)
+
+// domainName1 = 'net';
+// console.log(domainName1);
+// alert(domainName1)
+// document.write(domainName1)
+
+// domainName2 = 'com.ua';
+// console.log(domainName2);
+// alert(domainName2)
+// document.write(domainName2)
+
+// number1 = 5;
+// console.log(number1);
+// alert(number1)
+// document.write(number1)
+
+// number10 = 16;
+// console.log(number10);
+// alert(number10)
+// document.write(number10)
+
+// number = 2369;
+// console.log(number);
+// alert(number)
+// document.write(number)
+
+// number123 = 321;
+// console.log(number123);
+// alert(number123)
+// document.write(number123)
+
+// numberPi = 63.25;
+// console.log(numberPi);
+// alert(numberPi)
+// document.write(numberPi)
+
+// numberE = 298.1;
+// console.log(numberE);
+// alert(numberE)
+// document.write(numberE)
+
+// boolTrue = !true;
+// console.log(boolTrue);
+// alert(boolTrue)
+// document.write(boolTrue)
+
+// boolfalse = !false;
+// console.log(boolfalse);
+// alert(boolfalse)
+// document.write(boolfalse)
+
+//За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+ // Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+//  let name = prompt('Enter your name');
+//  let surName = prompt('Enter your surname');
+//  let age = prompt('Enter your age')
+//  console.log('Вітаю, ' + name + ' ' + surName + '.' + ' Tобі' + ' ' + age +' років');
+
+ //- Взяти змінні з завдання вище та зєднати їх в одну змінну (конкатинація).
+//  let userInfo = 'Вітаю, ' + name + ' ' + surName + '.' + ' Tобі' + ' ' + age +' років'
+//  alert(userInfo)
+
+//- За допомогою prompt() отримати 3 числа. Привести їх до числа (Google "String to number JS"). Вивести їх в консоль.
+
+// let number1 = prompt()
+// let number2 = prompt()
+// let number3 = prompt()
+
+// console.log(+number1);
+// console.log(+number2);
+// console.log(+number3);
+
+//За допомогою prompt() отримати 4 числа. Привести їх до числа за допомогою paeseInt. Додати їх всі мож собою і результат вивести в консоль.
+// let number1 = prompt()
+// let number2 = prompt()
+// let number3 = prompt()
+// let number4 = prompt()
+
+// let summ = parseInt(number1) + parseInt(number2) + parseInt(number3) + parseInt(number4)
+// console.log(summ);
+
+//За допомогою prompt() отримати 3 числа з плаваючою точкою за допомогою parseFloat.
+//Просумувати їх. Отрманий результат записати в змінну result. Вивести цю змінну в консоль. 
+
+// let number1 = prompt()
+// let number2 = prompt()
+// let number3 = prompt()
+
+// let result = parseFloat(number1) + parseFloat(number2) + parseFloat(number3)
+// console.log(result);
+
+//За допомогою prompt() отримати 3 числа з плаваючою точкою. Заокруглити їх (Google "round number JS"). Просумувати їх між собою. Результат вивести в консоль.
+
+// let number1 = prompt()
+// let number2 = prompt()
+// let number3 = prompt()
+
+// let result = Math.round(number1) + Math.round(number2) + Math.round(number3)
+// console.log(result);
+
+// За допомогою prompt() отримати 2 цілих числа.
+//   Перше число - це число яке будуть підносити до степеня. Друге число - це число яке являється степенню.
+//   За допомогою Math.pow (Google) піднести перше число в степінь другого числа.
+//   Example. Число 1 = 5. Число 2 = 2. Результат = 25 
+
+// let number1 = prompt()
+// let number2 = prompt()
+
+// let result = Math.pow(number1, number2)
+// console.log(result);
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//   let a = 100; 
+//   let b = '100'; 
+//   let c = true;
+
+//   console.log(typeof(a));
+//   console.log(typeof(b));
+//   console.log(typeof(c));
+
+// - Поставет відповідний оператор в виразах що б вийшов відповідний результат.
+//   В однакових виразаї не використовувати однакові оператори!!!
+    // 5 < 6 -> true
+    // 5 > 6 -> false
+    // 5 == 6 -> false
+    // 5 === 6 -> false
+    // 10 == 10 -> true
+    // 10 >= 10 -> true
+    // 10 > 10 -> false
+    // 10 < 10 -> false
+    // 10 != 10 -> false
+    // 123 === '123' -> false
+    // 123 == '123' -> true
+
+//     Додатково:
+// - Подивіться на наступні вирази і спробуйте вгадати яким буде вивід в консоль. Перевірте себе.
+
+  console.log(132 > 100 && 45 < 12 ); //falce
+  console.log(34 > 33 && 23 < 90 );  //true
+  console.log(99 > 100 && 45 > 12 ); //falce
+  console.log(132 > 100 || 45 < 12 ); //true
+  console.log(111 > 11 || 45 < 111 );  //true
+  console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) ); //true
+  console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );  //true
+  console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) ); //falce
+  console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 )); //true
+  console.log(!!'-1'); //true
+  console.log(!!-1); //true
+  console.log(!!'0'); //falce  помилився
+  console.log(!!'null'); //falce помилився
+  console.log(!!'undefined'); //falce помилився
+  console.log(!!(3/'owu')); //falce
+  console.log((111 > 11 || 45 < 111) ||  !!'0'); //true
+  console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));//falce
+ 
+
