a18de97bf7e9fda7388d63707866eda9dc90c170
OlehBabiak <oleg_bob@ukr.net>
2021-03-03 22:59:19 +0200

    add practice11

---
+++ Practice11_to-do-list/main.js
@@ -0,0 +1,53 @@
+
+let input = document.createElement('input')
+input.placeholder = 'enter item'
+input.id = '#txt'
+document.body.appendChild(input)
+
+let btn = document.createElement('button')
+btn.innerText = 'Add'
+document.body.appendChild(btn)
+
+let ul = document.createElement('ul')
+document.body.appendChild(ul)
+
+btn.addEventListener('click', function () {
+   
+        let li = document.createElement('li')
+        li.innerText = input.value
+        ul.appendChild(li)
+
+        let btn2 = document.createElement('button')
+        btn2.innerText = 'Delete'
+        li.appendChild(btn2)
+
+        btn2.addEventListener('click', function () {
+            li.classList.add('remove')
+        })
+
+        input.value = ''
+    
+    
+})
+
+
+
+input.addEventListener('keyup', function (ev) {
+
+ if (ev.which === 13) {
+      let li = document.createElement('li')
+      li.innerText = input.value
+      ul.appendChild(li)
+
+      let btn2 = document.createElement('button')
+      btn2.innerText = 'Delete'
+      li.appendChild(btn2)
+
+      btn2.addEventListener('click', function () {
+          li.classList.add('remove')
+      })
+
+      input.value = ''
+ }
+})
+
