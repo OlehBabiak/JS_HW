627126af5ae323a5b367f4dcd02c4c6954c8f0b5
OlehBabiak <oleg_bob@ukr.net>
2021-03-09 18:37:12 +0200

    kj

--- Lesson9_async/main.js
+++ Lesson9_async/main.js
@@ -30,35 +30,62 @@
 
 
 
-const text = 'Hello world!'
-let arr2 = text.split('')
+// const text = 'Hello world!'
+// let arr2 = text.split('')
 
-  let count = 0;
-  let string = '';
+//   let count = 0;
+//   let string = '';
  
-  function typeLine(cb) {
-    let interval = setTimeout(
-      () => {
-        string += text[count]
-        console.log(string);
-        document.querySelector('pre').innerHTML =string +'|';
+//   function typeLine(cb) {
+//     let interval = setTimeout(
+//       () => {
+//         string += text[count]
+//         console.log(string);
+//         document.querySelector('pre').innerHTML =string +'|';
 
-      count++;
-      console.log(count);
+//       count++;
+//       console.log(count);
 
-        if (count == text.length) {
-          clearTimeout(interval);
-           document.querySelector('pre').innerHTML =string;
-          return true;
-        }
+//         if (count == text.length) {
+//           clearTimeout(interval);
+//            document.querySelector('pre').innerHTML =string;
+//           return true;
+//         }
       
-      typeLine(delayInt);
-    }, cb(600))
-  }
+//       typeLine(delayInt);
+//     }, cb(600))
+//   }
 
-  typeLine(delayInt);
+//   typeLine(delayInt);
 
-function delayInt(delay) {
-  return Math.floor(Math.random() * delay);
+// function delayInt(delay) {
+//   return Math.floor(Math.random() * delay);
+// }
+
+// const writeCh = (ch) => new Promise(resolve => 
+//     setTimeout(() => {
+//     resolve(ch) 
+// }, Math.random() * 1000)
+// )
+
+
+
+
+
+
+const typeWriter = async (str) => {
+    for (let ch of str) {
+        console.log(await writeCh(ch))
+        
+    }
 }
 
+const writeCh = function (ch) {
+    return new Promise(resolve =>
+        setTimeout(() => {
+            resolve(ch)
+        }, Math.random() * 1000)
+    )
+}
+
+typeWriter('Hello world bro')
